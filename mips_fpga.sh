02b6e30a0a23540c861988ab7a0220e4af28c9e6
Manuel Ríos <mriosrivas@gmail.com>
2019-09-30 16:05:31 -0600

    Update mips_fpga.sh

--- mips_fpga.sh
+++ mips_fpga.sh
@@ -1,16 +1,33 @@
-#Create temp folder
-mkdir temp
-cd temp
+file="temp/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz"
+if [ -f "$file" ]
+then
+	echo "$file found."
+	cd temp
+	sudo mkdir /opt/imgtec/
+	sudo tar xvzf 'Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz' -C /opt/imgtec/
+	
+else
+	echo "$file not found."
+	mkdir temp
+    cd temp
 
-#Get Codescape toolchain Bare Metal
-wget https://codescape.mips.com/components/toolchain/2016.05-03/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz
-sudo mkdir /opt/imgtec/
-sudo tar xvzf Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz -C /opt/imgtec/
+    #Get Codescape toolchain Bare Metal
+    wget 'https://codescape.mips.com/components/toolchain/2016.05-03/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz'
+    sudo mkdir /opt/imgtec/
+    sudo tar xvzf 'Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Bare.Metal.CentOS-5.x86_64.tar.gz' -C /opt/imgtec/
+	    
+fi
 
 #libfdc Library
-sudo mkdir /opt/imgtec/mips-mti-elf/2016.05-03/share/mips/addons/
-unzip libfdc.zip -d /opt/imgtec/mips-mti-elf/2016.05-03/share/mips/addons/
+#sudo mkdir /opt/imgtec/mips-mti-elf/2016.05-03/share/mips/addons/
+
+cd ..
+sudo unzip libfdc.zip -d /opt/imgtec/mips-mti-elf/2016.05-03/share/mips/addons/libfdc/
 
 #Remove temp folder
 #cd ..
 #rm -r temp
+
+sudo echo 'export MIPS_ELF_ROOT="/opt/imgtec/mips-mti-elf/2016.05-03"' >> ~/.bashrc
+
+source ~/.bashrc
